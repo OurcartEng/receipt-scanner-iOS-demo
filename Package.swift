b05fa4f4b7f1a788ec89ddaa8b588bb5b541330c
Shpëtim Hoxha <shpetimhoxha3@gmail.com>
2024-12-09 11:46:30 +0100

    Bump Version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReceiptScannerLibrary",
-            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/0.5-beta/ReceiptScannerLibrary.xcframework.zip",
+            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.0.0/ReceiptScannerLibrary.xcframework.zip",
             checksum: "e644ac1357b4c87833ffeeab2155db4bc1fa6bdebbcaf95921c7e0d1eb57314c" 
         )
     ]
