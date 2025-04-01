578b1fca8512f6a51826315209371dfcfe202da3
sbx6 <shpetimhoxha3@gmail.com>
2025-04-01 12:21:32 +0200

    update checksum

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "ReceiptScannerLibrary",
             url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.0.3/ReceiptScannerLibrary.xcframework.zip",
-            checksum: "acc9dc6d55d173d372a663883ec6842d380f43d3bcbc791a4f7953485c656bbb"
+            checksum: "256eb4881d7a3459ecb3566d3513cc77c043625eef303153fba201134168da60"
         )
     ]
 )
