446676847318956a11945fcbc13fc02b52504fb9
sbx6 <shpetimhoxha3@gmail.com>
2025-05-08 14:38:56 +0200

    bump version to 1.0.10

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "TensorFlowLite",
-            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.0.10/TensorFlowLite.xcframework.zip",
+            name: "ReceiptScannerLibrary",
+            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.0.10/ReceiptScannerLibrary.xcframework.zip",
             checksum: "dcac6c45933f66a281322dc42b70c54765e1fc4c7b9c967b378de96f6d581f9a"
         ),
         .binaryTarget(
@@ -24,7 +24,7 @@ let package = Package(
             checksum: "748232b1eccd3ecd7be36f39d1a793580d2f3722ff22b9bf216d7e98128d615c"
         ),
         .binaryTarget(
-            name: "ReceiptScannerLibrary",
+            name: "TensorFlowLite",
             url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/tensorflow-binary/TensorFlowLite.xcframework.zip",
             checksum: "abaa4681aaef5c145ceda928a72a407b4e7d6a797e86e9afc44560ff5373d197"
         )
