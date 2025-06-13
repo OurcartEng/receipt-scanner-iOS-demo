b417c41890352028776a9c34937efa87bc4e9b0c
sbx6 <shpetimhoxha3@gmail.com>
2025-06-13 12:03:31 +0200

    release 1.1.2

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReceiptScannerLibrary",
-            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.1.1/ReceiptScannerLibrary.xcframework.zip",
-            checksum: "22da26cac09382aaf1de4b1ddb25781e2d84a0a9eac61ad551ec760f769603a5"
+            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.1.2/ReceiptScannerLibrary.xcframework.zip",
+            checksum: "ef385df51c1b5ab17c25d97139abb135d2215affc8cdf5dc3cd150f50ee05881"
         )
     ]
 )
