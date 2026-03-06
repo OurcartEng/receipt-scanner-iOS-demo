3d798046e3829542da5bb32666ab79c89f74ee0d
sbx6 <shpetimhoxha3@gmail.com>
2026-03-06 10:25:44 +0100

    Relase 1.13.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReceiptScannerLibrary",
-            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.13.0/ReceiptScannerLibrary.xcframework.zip",
-            checksum: "36e843e190fd9fa41ed8d318a496e607c318806cb5a701ee56233750b8fde5f5"
+            url: "https://sdk-library.s3.eu-central-1.amazonaws.com/iOS/1.13.1/ReceiptScannerLibrary.xcframework.zip",
+            checksum: "440e069f5f7d1038a3923913a89b21ad51765d5c8bbf72a1f19e3f2c099927c5"
         )
     ]
 )
